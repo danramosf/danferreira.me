fe0ad8184ffd09c0645513aa28d2be72f66a0f9e
danramosf <danramosf@gmail.com>
2019-02-18 17:08:37 -0500

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,25 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'danferreira';
+  lat: number = 43.2386344;
+  lng: number = -79.7205817;
+
+   // Used to toggle the menu on small screens when clicking on the menu button
+   toggleFunction() {
+      var x = document.getElementById("navDemo");
+      if (x.className.indexOf("w3-show") == -1) {
+        x.className += " w3-show";
+      } else {
+        x.className = x.className.replace(" w3-show", "");
+      }
+   }
+
+  //// Modal Image Gallery
+  //onClick(element) {
+  //  document.getElementById("img01").src = element.src;
+  //  document.getElementById("modal01").style.display = "block";
+  //  var captionText = document.getElementById("caption");
+  //  captionText.innerHTML = element.alt;
+  //}
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { AgmCoreModule } from '@agm/core';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AgmCoreModule.forRoot({
+      apiKey: 'AIzaSyAKTqj2sK9OT-mdGCoRjQ-DGrdzKBg3m8Y'
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
